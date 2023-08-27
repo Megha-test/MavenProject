7f8846dee7e38290658f6d676264e519e0f4c99b
Darshan Avanthkar <Darshan Avanthkar@DESKTOP-80OMUG7>
2023-08-27 14:00:50 +0530

    commit

---
+++ NewMavenP/src/test/java/com/actitime/testscript/TASK.java
@@ -0,0 +1,11 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TASK {
+	@Test
+public void deleteTask() {
+	Reporter.log("deleteTask",true);
+}
+}
